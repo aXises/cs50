1b64f5c7da276a4195af2f8a016a197195e87dc0
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-01-07 10:56:56 +1000

    Mario.c

---
+++ pset1/mario/more/mario.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    
+    int height = get_int("Height: ");
+    
+    while (height < 0 || height > 23)
+    {
+        height = get_int("Height: ");
+    };
+    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = height; j > 0; j--)
+        {
+            if (i + 1 > j - 1)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        
+        printf("  ");
+        
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    };
+    
+};
